245ec49f50463783b9f07966a5e3e7a3dfa99c58
Michael S. P. Kelley <msk@astro.umd.edu>
2025-05-15 09:06:50 -0400

    Begin edits to harvest sw from the PSI server.

--- cs_harvester/network.py
+++ cs_harvester/network.py
@@ -13,6 +13,17 @@ user_agent = f"CATCH-SIS Harvester {__version__}"
 
 @contextmanager
 def session():
+    """Set HTTP User-Agent in a requests session.
+
+
+    Example
+    -------
+
+    >>> with session() as req:
+    ...     req.get("https://pdssbn.astro.umd.edu/")
+
+    """
+
     with req.Session() as s:
         s.headers.update({"User-Agent": user_agent})
         yield s
@@ -20,5 +31,17 @@ def session():
 
 @contextmanager
 def set_astropy_useragent():
+    """Set astropy's HTTP User-Agent.
+
+
+    Example
+    -------
+
+    >>> from astropy.io import fits
+    >>> with set_astropy_useragent():
+    ...     fits.open("https://pdssbn.astro.umd.edu/holdings/ear-c-ccd-3-edr-halley-outburst-uh-v1.0/data/19910412/uh00896.fit")
+
+    """
+
     with astropy_conf.set_temp("default_http_user_agent", user_agent):
         yield
