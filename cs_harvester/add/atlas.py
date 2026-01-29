dd26a8f32663bfccc18eedebb93ad1356e35735b
Michael S. P. Kelley <msk@astro.umd.edu>
2026-01-28 19:41:45 -0500

    Also raise error with location and night number

--- cs_harvester/add/atlas.py
+++ cs_harvester/add/atlas.py
@@ -100,8 +100,15 @@ def latest_collection(files):
 
 
 def find_collection(location: str, night_number: int) -> StructureList:
-    files = glob(f"/n/{location}/collection_{night_number}*.xml")
-    return latest_collection(files)
+    try:
+        files = glob(f"/n/{location}/collection_{night_number}*.xml")
+        return latest_collection(files)
+    except ValueError as exc:
+        raise ValueError(
+            "Could not find collection for location {}, night number {}".format(
+                location, night_number
+            )
+        )
 
 
 def get_observation(catch, label) -> ATLAS:
